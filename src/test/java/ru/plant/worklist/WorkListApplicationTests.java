ebf63fa32f99ea8fa9106f70b5b215b67253b96c
Ivan.Vereschagin <Ivan.Vereschagin@casceng.com>
2024-03-29 20:55:54 +0300

    init

---
+++ src/test/java/ru/plant/worklist/WorkListApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.plant.worklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
